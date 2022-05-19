672b6b55d7cc4d636fc13c35ea83c44623278bb9
Marie Eve Frechette <marevedesign@gmail.com>
2022-05-19 14:16:49 -0400

    Prep des fonctions a faire

--- Controllers/homeComtroller.js
+++ Controllers/homeComtroller.js
@@ -20,3 +20,65 @@ const Review = require("../Models/review");
 
 //------------------------------------------------------------------------------------------------------------------//
 
+exports.getMovies = (req, res) => {
+    res.render("movies");
+}
+
+exports.redirectIndex = (req, res) => {
+    res.redirect("/");
+}
+
+exports.getAPIMovies = ("/movies", (req, res) => {
+    response.json(resulat);
+    //avec le then et le catch, et on fait pas du render mais du json si on fait apparaitre du data.
+    //on va faire apparaitre le data de tous les films ici.
+    //il manque pleins de trucs ici lol
+});
+
+exports.getTitles = (req, res) => {
+    response.json(resulat);
+    //avec le then et le catch, et on fait pas du render mais du json si on fait apparaitre du data.
+    //on va faire apparaitre le data de tous les titres ici.
+    //on va utiliser le params
+    //il manque pleins de trucs ici lol
+};
+
+exports.getRates = (req, res) => {
+    response.json(resulat);
+    //avec le then et le catch, et on fait pas du render mais du json si on fait apparaitre du data.
+    //on va faire apparaitre le data de tous les rates ici.
+    //on va utiliser le params
+    //il manque pleins de trucs ici lol
+};
+
+exports.moviesPerPage = (req, res) => {
+    response.json(resulat);
+    //avec le then et le catch, et on fait pas du render mais du json si on fait apparaitre du data.
+    //on va faire apparaitre le data du nombres de films par page ici. Defaut = 20
+    //on va utiliser le params ??
+    //il manque pleins de trucs ici lol
+};
+
+exports.getPages = (req, res) => {
+    response.json(resulat);
+    //avec le then et le catch, et on fait pas du render mais du json si on fait apparaitre du data.
+    //on va faire apparaitre le data du nombres de pages
+    //on va utiliser le params ??
+    //il manque pleins de trucs ici lol
+};
+
+exports.getRatings = (req, res) => {
+    response.json(resulat);
+    //avec le then et le catch, et on fait pas du render mais du json si on fait apparaitre du data.
+    //on va faire apparaitre le data de tous les ratings dispo
+    //on va utiliser le params ??
+    //il manque pleins de trucs ici lol
+};
+
+exports.getMovieID = (req, res) => {
+    response.json(resulat);
+    //avec le then et le catch, et on fait pas du render mais du json si on fait apparaitre du data.
+    //on va faire apparaitre le data avec l'aide d'un ID de film en particulier
+    //on va utiliser le params
+    //il manque pleins de trucs ici lol
+};
