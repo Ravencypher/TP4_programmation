8941548853dc99c0ff8d740828c74ac236ceff42
Marie Eve Frechette <marevedesign@gmail.com>
2022-05-26 17:36:28 -0400

    Version finale

--- Controllers/homeComtroller.js
+++ Controllers/homeComtroller.js
@@ -1,82 +1,101 @@
 const Movie = require("../Models/movies");
 const Comment = require("../Models/comments");
 
-//------------------------------------------------------------------------------------------------------------------//
-// Regular routes
+// .limit et .skip permettent ici de limiter à 20 films à la page '0' (c'est-à-dire, au film #1 et suivant...)
+// Si nous voulons 'skipper' vers les 20 suivants, alors nous devons mettre .skip à 20. (et donc avoir le film #21 et suivant...)
+let limit = 20;
+let skip = 0;
 
-exports.getIndex = (req, res) => {
-    res.render("index");
-}
+const moviesRating = {
+    movies: ["AO", "APPROVED", "Approved", "G", "GP", "M", "NC-17", "NOT RATED", "Not Rated", "OPEN", "PASSED", "PG", "PG-13", "R", "TV-14", "TV-G", "TV-MA", "TV-PG", "TV-Y7", "UNRATED", "X"]
+};
 
 exports.getRedirect = (req, res) => {
     res.redirect('/api/v1/movies');
-}
-
-exports.getMovies = (req, res) => {
-    res.render("movies");
 }
 
 //------------------------------------------------------------------------------------------------------------------//
 //Find exact movie id
 
 exports.getMovieID = (req, res) => {
-    const searchById = {
-        _id: req.params.id
-    };
+    const searchById = {_id: req.params.id};
     Movie.findById(searchById)
         .then(result => {
-            console.log("Search query for the movie ID is : " + req.params.id)
+            console.log(`Search query for the movie ID is : ${req.params.id}`)
             res.json(result);
         })
         .catch(error => {
-            console.log("API isn't available. There was an error. Error is : " + error)
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
         })
 };
 
 //------------------------------------------------------------------------------------------------------------------//
-// Find an exact title or find all titles if keyword is generic (exemple : man gets many results, while Queen%20Kelly gets one specific result).
-// ATTN: change the space by %20 for testing directly in the browser if title is specific.
+// Find an exact title or find all titles if keyword is generic (example : man gets many results, while Queen%20Kelly gets one specific result).
 
 exports.getTitle = (req, res) => {
-    Movie.find({
-            title: {
-                $regex: req.params.title
-            }
-        })
+    Movie.find({title: {$regex: req.params.title}})
         .then(result => {
-            console.log("Search query for the title is : " + req.params.title)
+            console.log(`Search query for the title is : ${req.params.title}`)
             res.json(result);
         })
         .catch(error => {
-            console.log("API isn't available. There was an error. Error is : " + error)
-        })
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
+        });
 };
 
 //------------------------------------------------------------------------------------------------------------------//
-//Find all the data available in the API (Over 24,000 results)
+// Compare the amount of objects in the title result. If length is 0, then it searches by rated instead.
 
-// Point 3 de la section 2 du PDF :
-// Par ma compréhension du PDF, c'est un .limit.
-// Si le user ne donne pas un nombre de films, alors le .limit sera de 20. Si le user spécifie, par exemple 5 films, alors le .limit sera de 5.
+exports.getTitleOrRates = (req, res) => {
+    Movie.find({title: {$regex: req.params.title}}).limit(limit).skip(skip)
+        .then(result => {
+            if (result.length !== 0) {
+                console.log(`Search query for the title is : ${req.params.title}`)
+                console.log(`Amount of results is : ${result.length}`)
+                res.json(result)
+            } else {
+                Movie.find({rated: req.params.title}).limit(limit)
+                    .then(result => {
+                        console.log(`No results for the title you're looking for (${req.params.title}). Going into rated instead.`)
+                        console.log(`Results are for the search by rated : ${req.params.title}`)
+                        res.json(result)
+                    })
+            }
+        })
+        .catch(error => {
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
+        });
+}
 
-// Point 4 de la section 2 du PDF :
-// .limit et .skip permettent ici de limiter à 20 films à la page '0' (c'est-à-dire, au film #1 et suivant...)
-// Si nous voulons 'skipper' vers les 20 suivants, alors nous devons mettre .skip a 20. (et donc avoir le film #21 et suivant...)
+//------------------------------------------------------------------------------------------------------------------//
+// Search for a movie title, with a keyword, and add a rating.
 
-const limit = 20;
-const skip = 20;
+exports.getTitleThenRates = (req, res) => {
+    Movie.find({
+            "title": {$regex: req.params.title},
+            "rated": req.params.rated
+            }).limit(limit).skip(skip)
+
+        .then(result => {
+            console.log(`Search query for the title is : ${req.params.title} with a rating of ${req.params.rated}`)
+            res.json(result)
+        })
+        .catch(error => {
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
+        });
+}
+
+//------------------------------------------------------------------------------------------------------------------//
+// Find all the data available in the API (23,000 + results)
 
 exports.getAPIMovies = (req, res) => {
-    Movie.find({
-            data: req.data
-        })
-        .limit(limit).skip(skip)
+    Movie.find({data: req.data})
         .then(result => {
-            console.log("data is loading... Please be patient !")
+            console.log(`Data is loading... Please be patient ! Currently ${result.length} Results loaded.`)
             res.json(result);
         })
         .catch(error => {
-            console.log("API isn't available. There was an error. Error is : " + error)
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
         })
 };
 
@@ -84,38 +103,20 @@ exports.getAPIMovies = (req, res) => {
 // Find all ratings (keyword 'rated' in the API)
 
 exports.getRated = (req, res) => {
-    Movie.find({
-            rated: {
-                $regex: req.params.rated
-            }
-        })
+    Movie.find({rated: {$regex: req.params.rated}}).limit(limit).skip(skip)
         .then(result => {
-            console.log("Search query for the rating is : " + req.params.rated)
+            console.log(`Search query for the rating is : ${req.params.rated}`)
             res.json(result);
         })
         .catch(error => {
-            console.log("API isn't available. There was an error. Error is : " + error)
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
         })
 };
 
 //------------------------------------------------------------------------------------------------------------------//
 // getRating sends all the ratings available.
 
-const moviesRating = {
-    movies: ["AO", "APPROVED", "Approved", "G", "GP", "M", "NC-17", "NOT RATED", "Not Rated", "OPEN", "PASSED", "PG", "PG-13", "R", "TV-14", "TV-G", "TV-MA", "TV-PG", "TV-Y7", "UNRATED", "X"]
-};
-
 exports.getRating = (req, res) => {
     moviesRating;
     res.json(moviesRating);
 };
-
-//------------------------------------------------------------------------------------------------------------------//
-
-// exports.moviesPerPage = (req, res) => {
-//     res.json("moviesPerPage non defini encore");
-// };
-
-// exports.getPages = (req, res) => {
-//     res.json("getPages non defini encore");
-// };
--- Routes/moviesRoutes.js
+++ Routes/moviesRoutes.js
@@ -1,43 +1,27 @@
 const express = require("express");
+const app = express();
 const router = express.Router();
 const homeController = require("../Controllers/homeComtroller");
 
-//------------------------------------------------------------------------------------------------------------------//
-//MOVIE ROUTES OK//
-
-// Va faire une redirection de /api/v1/movies sur le slash
 router.get("/api/v1/movies", homeController.getAPIMovies);
 router.get("/", homeController.getRedirect);
 
-// va sur une pseudo page d'accueil
-router.get("/index", homeController.getIndex);
-
-// Va direct sur une page de recherche de films
-router.get("/movies", homeController.getMovies);
-
-// Get a specific movie ID
+// Get a specific movie by ID
 router.get("/api/v1/movies/id/:id", homeController.getMovieID);
 
-// Get movie title(s)
-router.get("/api/v1/movies/titles/:title", homeController.getTitle);
+// Get movie title(s) along with a particular rating.
+router.get("/api/v1/movies/sortwithfilter=:title/:rated", homeController.getTitleThenRates);
 
-// Get movie rating(s)
-router.get("/api/v1/movies/rated/:rated", homeController.getRated);
+// Get movie title(s), if the title doesn't exit, then it will search by rating.
+router.get("/api/v1/movies/sort=:title", homeController.getTitleOrRates);
+
+// Get movie title(s) only
+router.get("/api/v1/movies/titles=:title", homeController.getTitle);
+
+// Get movie rating(s) only
+router.get("/api/v1/movies/rated=:rated", homeController.getRated);
 
 // Get all the ratings available
 router.get("/api/v1/movies/rating", homeController.getRating);
-
-//------------------------------------------------------------------------------------------------------------------//
-//MOVIE ROUTES A FAIRE//
-
-// moviesPerPage=nombreMovies qui permet de retourner seulement nombreMovies movies.
-// Si cette nombreMovies n’est pas fournie seulement 20 movies seront retourner. (j'ai mis 20)
-// router.get("/api/v1/movies/titles/:title?limit=20", homeController.moviesPerPage);
-
-// pages=nombrePage qui permet de retourner le nombreMovies du nombrePage page. La valeur par defaut est 0.
-// Aide : utilisez la methode .limit(MoviesPerPage).skip(MoviesPerPage*pages) pour retourner le nombreMovies du nombrePage page.
-// router.get("/api/v1/movies/:pages=nombrePage", homeController.getPages);
-
-//------------------------------------------------------------------------------------------------------------------//
 
 module.exports = router;
