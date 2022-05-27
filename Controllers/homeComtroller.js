1f4b3a41ce7cc0c97d4823ce906288cd290d0b67
Marie Eve Frechette <marevedesign@gmail.com>
2022-05-27 11:08:01 -0400

    Cleaning dans le main.js et homeController. delete du styles.css vu qu'on a pas de views et public.

--- Controllers/homeComtroller.js
+++ Controllers/homeComtroller.js
@@ -1,6 +1,3 @@
-const Movie = require("../Models/movies");
-const Comment = require("../Models/comments");
-
 // .limit et .skip permettent ici de limiter à 20 films à la page '0' (c'est-à-dire, au film #1 et suivant...)
 // Si nous voulons 'skipper' vers les 20 suivants, alors nous devons mettre .skip à 20. (et donc avoir le film #21 et suivant...)
 let limit = 20;
@@ -10,12 +7,29 @@ const moviesRating = {
     movies: ["AO", "APPROVED", "Approved", "G", "GP", "M", "NC-17", "NOT RATED", "Not Rated", "OPEN", "PASSED", "PG", "PG-13", "R", "TV-14", "TV-G", "TV-MA", "TV-PG", "TV-Y7", "UNRATED", "X"]
 };
 
+const Movie = require("../Models/movies");
+const Comment = require("../Models/comments");
+
+//------------------------------------------------------------------------------------------------------------------//
+// Find all the data available in the API (23,000 + results)
+
 exports.getRedirect = (req, res) => {
     res.redirect('/api/v1/movies');
 }
 
+exports.getAPIMovies = (req, res) => {
+    Movie.find({data: req.data})
+        .then(result => {
+            console.log(`Data is loading... Please be patient ! Currently loading ${result.length} results.`)
+            res.json(result);
+        })
+        .catch(error => {
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
+        })
+};
+
 //------------------------------------------------------------------------------------------------------------------//
-//Find exact movie id
+//Find a movie with it's ID
 
 exports.getMovieID = (req, res) => {
     const searchById = {_id: req.params.id};
@@ -30,10 +44,11 @@ exports.getMovieID = (req, res) => {
 };
 
 //------------------------------------------------------------------------------------------------------------------//
-// Find an exact title or find all titles if keyword is generic (example : man gets many results, while Queen%20Kelly gets one specific result).
+// Find an exact title or find all titles if keyword is generic (example : "man" gets many results, while "Queen%20Kelly" gets one specific result).
+// limit & skip applied.
 
 exports.getTitle = (req, res) => {
-    Movie.find({title: {$regex: req.params.title}})
+    Movie.find({title: {$regex: req.params.title}}).limit(limit).skip(skip)
         .then(result => {
             console.log(`Search query for the title is : ${req.params.title}`)
             res.json(result);
@@ -43,64 +58,10 @@ exports.getTitle = (req, res) => {
         });
 };
 
-//------------------------------------------------------------------------------------------------------------------//
-// Compare the amount of objects in the title result. If length is 0, then it searches by rated instead.
-
-exports.getTitleOrRates = (req, res) => {
-    Movie.find({title: {$regex: req.params.title}}).limit(limit).skip(skip)
-        .then(result => {
-            if (result.length !== 0) {
-                console.log(`Search query for the title is : ${req.params.title}`)
-                console.log(`Amount of results is : ${result.length}`)
-                res.json(result)
-            } else {
-                Movie.find({rated: req.params.title}).limit(limit)
-                    .then(result => {
-                        console.log(`No results for the title you're looking for (${req.params.title}). Going into rated instead.`)
-                        console.log(`Results are for the search by rated : ${req.params.title}`)
-                        res.json(result)
-                    })
-            }
-        })
-        .catch(error => {
-            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
-        });
-}
-
-//------------------------------------------------------------------------------------------------------------------//
-// Search for a movie title, with a keyword, and add a rating.
-
-exports.getTitleThenRates = (req, res) => {
-    Movie.find({
-            "title": {$regex: req.params.title},
-            "rated": req.params.rated
-            }).limit(limit).skip(skip)
-
-        .then(result => {
-            console.log(`Search query for the title is : ${req.params.title} with a rating of ${req.params.rated}`)
-            res.json(result)
-        })
-        .catch(error => {
-            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
-        });
-}
-
-//------------------------------------------------------------------------------------------------------------------//
-// Find all the data available in the API (23,000 + results)
-
-exports.getAPIMovies = (req, res) => {
-    Movie.find({data: req.data})
-        .then(result => {
-            console.log(`Data is loading... Please be patient ! Currently ${result.length} Results loaded.`)
-            res.json(result);
-        })
-        .catch(error => {
-            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
-        })
-};
 
 //------------------------------------------------------------------------------------------------------------------//
 // Find all ratings (keyword 'rated' in the API)
+// limit & skip applied.
 
 exports.getRated = (req, res) => {
     Movie.find({rated: {$regex: req.params.rated}}).limit(limit).skip(skip)
@@ -120,3 +81,49 @@ exports.getRating = (req, res) => {
     moviesRating;
     res.json(moviesRating);
 };
+
+//------------------------------------------------------------------------------------------------------------------//
+// Compare the amount of objects in the title result. If length is 0, then it searches by rated instead.
+// limit & skip applied.
+
+exports.getTitleOrRates = (req, res) => {
+    Movie.find({title: {$regex: req.params.title}}).limit(limit).skip(skip)
+        .then(result => {
+            if (result.length !== 0) {
+                console.log(`Search query for the title is : ${req.params.title}`)
+                console.log(`Amount of results is : ${result.length}`)
+                res.json(result)
+            } else {
+                Movie.find({rated: req.params.title}).limit(limit)
+                    .then(result => {
+                        console.log(`No results for the title you're looking for (${req.params.title}). Going into "rated" instead.`)
+                        console.log(`Results are for the search by "rated" : ${req.params.title}`)
+                        res.json(result)
+                    })
+            }
+        })
+        .catch(error => {
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
+        });
+}
+
+//------------------------------------------------------------------------------------------------------------------//
+// Search for a movie title, with a keyword, and add a rating.
+// limit & skip applied.
+
+exports.getTitleThenRates = (req, res) => {
+    Movie.find({
+            "title": {$regex: req.params.title},
+            "rated": req.params.rated
+            }).limit(limit).skip(skip)
+
+        .then(result => {
+            console.log(`Search query for the title is : ${req.params.title} with a rating of ${req.params.rated}`)
+            res.json(result)
+        })
+        .catch(error => {
+            console.log(`API isn't available. There was an error. Error is message is : ${error.message}`)
+        });
+}
+
+//------------------------------------------------------------------------------------------------------------------//
--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const app = express();
-
 const dotenv = require('dotenv');
-dotenv.config({
-    path: './config.env'
-});
-
+dotenv.config({path: './config.env'});
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+
 const dbPath = process.env.DB_PATH;
-const dbOptions = {
-    useNewUrlParser: true
-};
+const dbOptions = {useNewUrlParser: true};
 const port = process.env.PORT || 5000;
 
 const mainRouter = require('./Routes/moviesRoutes');
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(express.static('./public'));
-app.use(express.urlencoded({
-    extended: true
-}));
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(methodOverride('_method'));
 
